3dd5e23f16052e7748dd52d1fa8fe0c2bec80478
Gerald <geraldarzola@gmail.com>
2021-01-14 01:53:25 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES(1,"Morgan", 25, "M", "red", "calm", 1);
+INSERT INTO bears VALUES(2,"Eve", 19, "F", "brown", "cool", 1);
+INSERT INTO bears VALUES (3,"Arzy", 22 , "M", "black", "collected",1);
+INSERT INTO bears VALUES(4,"Mark", 22 , "M", "black", "calm",1);
+INSERT INTO bears VALUES(5,"George", 100, "M", "white", "collected", 0);
+INSERT INTO bears VALUES(6,"Santa", 100, "M", "white", "cool", 1);
+INSERT INTO bears VALUES(7,"Amanda", 32, "F", "orange" ,"cool", 0);
+INSERT INTO bears VALUES(8,null, 24, "F", "brown","collected", 1 );
